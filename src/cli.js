975239e61be6fede567c65352a97a5fea1159018
root <root@MSI>
2022-12-31 18:51:10 +0300

    step 4 add eslint

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
-const greetUser = () =>{
-    var userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`)
-}
+const greetUser = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
 
-export{greetUser};
+export { greetUser };
