fe7ba49a69adf84a1708e2f0e0ef9e077f479821
root <root@MSI>
2023-01-03 20:46:21 +0300

    update prame-even-modules.js

--- src/prime-even-modules.js
+++ src/prime-even-modules.js
@@ -12,7 +12,7 @@ function findPrime(n) {
   if (n <= 1) {
     return 'no';
   }
-  for (let i = 2; i < n; i+=1) {
+  for (let i = 2; i < n; i += 1) {
     if (n % i === 0) {
       return 'no';
     }
@@ -42,7 +42,7 @@ export default function startGame(str) {
     const checkedAnswer = checkAnswer(userAnswer, correctAnswer, userName);
     if (checkedAnswer === false) return;
 
-    i+=1;
+    i += 1;
   }
 
   console.log(`Congratulations, ${userName}!`);
