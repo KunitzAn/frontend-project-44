8d17d530838f3fbae2ff44181349182cab329238
root <root@MSI>
2023-01-05 13:09:00 +0300

    add gcd.js and update brain-gcd.js

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,34 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import greetUser from '../src/cli.js';
-import { checkAnswer, generateRandomInt } from '../src/modules.js';
+import startGame from '../src/games/gcd.js';
 
-const calculate = (x, y) => {
-  if (y > x) return calculate(y, x);
-  if (!y) return x;
-  return calculate(y, x % y);
-};
-
-const startGameGcd = () => {
-  const userName = greetUser();
-  console.log('Find the greatest common divisor of given numbers.');
-
-  let i = 0;
-  while (i < 3) {
-    const int1 = generateRandomInt();
-    const int2 = generateRandomInt();
-
-    const curAnswer = readlineSync.question(`Question: ${int1} ${int2} \nYour answer: `);
-    const userAnswer = +curAnswer;
-    const correctAnswer = calculate(int1, int2);
-
-    const checkedAnswer = checkAnswer(userAnswer, correctAnswer, userName);
-    if (checkedAnswer === false) return;
-
-    i += 1;
-  }
-
-  console.log(`Congratulations, ${userName}!`);
-};
-
-startGameGcd();
+startGame();
--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,1 +1,24 @@
 import startGame from '../index.js';
+import generateRandomInt from '../generateRandomInt.js';
+
+const task = 'Find the greatest common divisor of given numbers.';
+
+const calculate = (x, y) => {
+  if (y > x) return calculate(y, x);
+  if (!y) return x;
+  return calculate(y, x % y);
+};
+
+const runTheGame = () => {
+  const int1 = generateRandomInt();
+  const int2 = generateRandomInt();
+
+  const question = `${int1} ${int2}`;
+  const answer = calculate(int1, int2);
+
+  return [question, answer];
+};
+
+export default () => {
+  startGame(task, runTheGame);
+};
