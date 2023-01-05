d1a09eb7351960f70db821a22513423a50472d9e
root <root@MSI>
2023-01-05 13:22:15 +0300

    add progression.js and update brain-progression.js

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,47 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import greetUser from '../src/cli.js';
-import { checkAnswer } from '../src/modules.js';
+import startGame from '../src/games/progression.js';
 
-const generateRandomIntBetween = (min, max) => Math.floor(Math.random() * (max - min) + min);
-
-const generateProgression = () => {
-  const result = [];
-  const gradient = generateRandomIntBetween(1, 11);
-  let i = 1;
-  const progressionLength = generateRandomIntBetween(5, 11);
-  while (i <= progressionLength) {
-    result.push(gradient * i);
-    i += 1;
-  }
-  return result;
-};
-
-const startGameProgression = () => {
-  const userName = greetUser();
-  console.log('What number is missing in the progression?');
-
-  let i = 0;
-  while (i < 3) {
-    const progression = generateProgression();
-    const missedIntInd = generateRandomIntBetween(1, progression.length);
-    const correctAnswer = progression[missedIntInd - 1];
-    progression[missedIntInd - 1] = '..';
-
-    let str = `${progression[0]}`;
-    for (let k = 1; k < progression.length; k += 1) {
-      str = `${str} ${progression[k]}`;
-    }
-
-    const curAnswer = readlineSync.question(`Question: ${str} \nYour answer: `);
-    const userAnswer = +curAnswer;
-    const checkedAnswer = checkAnswer(userAnswer, correctAnswer, userName);
-    if (checkedAnswer === false) {
-      return;
-    }
-    i += 1;
-  }
-  console.log(`Congratulations, ${userName}!`);
-};
-
-startGameProgression();
+startGame();
---
+++ src/games/progression.js
@@ -0,0 +1,35 @@
+import startGame from '../index.js';
+
+const task = 'What number is missing in the progression?';
+
+const generateRandomIntBetween = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+const generateProgression = () => {
+  const result = [];
+  const gradient = generateRandomIntBetween(1, 11);
+  let i = 1;
+  const progressionLength = generateRandomIntBetween(5, 11);
+  while (i <= progressionLength) {
+    result.push(gradient * i);
+    i += 1;
+  }
+  return result;
+};
+
+const runTheGame = () => {
+  const progression = generateProgression();
+  const missedIntInd = generateRandomIntBetween(1, progression.length);
+  const answer = progression[missedIntInd - 1];
+  progression[missedIntInd - 1] = '..';
+  let str = `${progression[0]}`;
+  for (let k = 1; k < progression.length; k += 1) {
+    str = `${str} ${progression[k]}`;
+  }
+  const question = `${str}`;
+
+  return [question, answer];
+};
+
+export default () => {
+  startGame(task, runTheGame);
+};
